2098eb15f807f662fe5b5a0fcd34a12803d1bb33
Hào Chu Quang <chuhao878@gmail.com>
2023-07-16 09:53:40 +0700

    Update create-api-permission

--- src/services/permission.service.js
+++ src/services/permission.service.js
@@ -3,3 +3,21 @@ import Permission from "../models/permission.model";
 export const getAll = async () => {
   return Permission.find();
 };
+
+export const getById = async (id) => {
+  return Permission.findById(id);
+};
+
+export const create = async (data) => {
+  return Permission.create(data);
+};
+
+export const update = async (id, data) => {
+  return Permission.findByIdAndUpdate(id, data, {
+    new: true,
+  });
+};
+
+export const remove = async (id) => {
+  return Permission.findByIdAndDelete(id);
+};
